9b172bcf14eed43bd553a67a91e8a5d8b0ade7fb
a-humble-boy <1761418780@qq.com>
2022-02-08 16:30:23 +0800

    hot-fix 1 commit

--- src/main/java/com/entor/RedisStarter.java
+++ src/main/java/com/entor/RedisStarter.java
@@ -8,7 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @MapperScan(basePackages = "com.entor.mapper")
 public class RedisStarter {
     /**
-     * 启动类
+     * 启动类 hot-fix
      * @param args
      */
     public static void main(String[] args) {
